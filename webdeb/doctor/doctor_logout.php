aaf974a21db24326d011d670b9d04aec7e56e291
giloooo <93816287+giloooo@users.noreply.github.com>
2022-07-17 18:21:24 +0800

    Add files via upload

---
+++ webdeb/doctor/doctor_logout.php
@@ -0,0 +1,12 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['email']) && !isset($_SESSION['password']))
+	{
+		header('location: ../index.php');
+	}
+	else
+	{
+		session_destroy();
+		header('location: doctor_login.php');
+	}
+?>
